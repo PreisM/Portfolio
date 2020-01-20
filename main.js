291302589ad2d16430b4e525c72c3e8e868af7be
PreisM <59081755+PreisM@users.noreply.github.com>
2020-01-20 23:10:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function() {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 50
+        }, 1500);
+    });
+
+    $('#up').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+
+    })
+
+});
